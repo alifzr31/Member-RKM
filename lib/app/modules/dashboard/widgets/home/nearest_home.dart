4cfb7f720954a3332404f3c19cbb12654e4668ee
alifzr31 <alifzakya.r@gmail.com>
2024-01-15 16:38:59 +0700

    give condition if gps not active and don't have permission

--- lib/app/modules/dashboard/widgets/home/nearest_home.dart
+++ lib/app/modules/dashboard/widgets/home/nearest_home.dart
@@ -1,11 +1,15 @@
+import 'package:boxicons/boxicons.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:member_rkm/app/components/base_bottomsheet.dart';
 import 'package:member_rkm/app/components/base_shimmer.dart';
 import 'package:member_rkm/app/components/base_text.dart';
 import 'package:member_rkm/app/core/values/app_helpers.dart';
 import 'package:member_rkm/app/modules/dashboard/components/home/nearest_box.dart';
 import 'package:member_rkm/app/modules/dashboard/controller.dart';
 import 'package:member_rkm/app/core/values/colors.dart';
+import 'package:member_rkm/app/modules/dashboard/widgets/locationdesc_widget.dart';
 
 class NearestHome extends StatelessWidget {
   NearestHome({super.key});
@@ -56,49 +60,92 @@ class NearestHome extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             Expanded(
-              child: controller.storeLoading.value
-                  ? ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return const BaseShimmer(
-                          child: NearestBox(
-                            storeName: '',
-                            address: '',
-                            distance: '',
+              child: controller.servicestatus.isFalse ||
+                      controller.haspermission.isFalse
+                  ? Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Boxicons.bxs_map,
+                            size: 40,
                           ),
-                        );
-                      },
-                    )
-                  : ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: controller.store.length > 2
-                          ? controller.store.sublist(0, 3).length
-                          : controller.store.length,
-                      itemBuilder: (context, index) {
-                        final store = controller.store.length > 2
-                            ? controller.store.sublist(0, 3)[index]
-                            : controller.store[index];
-                        final distance =
-                            AppHelpers.decimalTwoDigits(store.distance ?? '0');
+                          const SizedBox(height: 5),
+                          TextButton.icon(
+                            onPressed: () async {
+                              controller.servicestatus.value =
+                                  await Geolocator.isLocationServiceEnabled();
+                              final permission =
+                                  await Geolocator.checkPermission();
 
-                        return NearestBox(
-                          storeName: store.name ?? '',
-                          address: store.address ?? '',
-                          distance: distance,
-                          onTap: () {
-                            controller.currentIndex.value = 2;
-                            controller.boxController.value.closeBox();
-                            controller.selectedLat.value =
-                                double.parse(store.lat ?? '');
-                            controller.selectedLong.value =
-                                double.parse(store.long ?? '');
-                            controller.moveStore();
+                              if (controller.servicestatus.value) {
+                                if (permission == LocationPermission.denied) {
+                                  baseBottomSheet(280, LocationDescWidget());
+                                } else if (permission ==
+                                    LocationPermission.deniedForever) {
+                                  baseBottomSheet(280, LocationDescWidget());
+                                } else {
+                                  controller.fetchLocation();
+                                }
+                              } else {
+                                baseBottomSheet(280, LocationDescWidget());
+                              }
+                            },
+                            icon: const Icon(Boxicons.bx_refresh),
+                            label: BaseText(
+                              text: controller.servicestatus.isFalse
+                                  ? 'Nyalakan GPS'
+                                  : controller.haspermission.isFalse
+                                      ? 'Berikan Izin'
+                                      : 'Muat Ulang',
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : controller.storeLoading.value
+                      ? ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return const BaseShimmer(
+                              child: NearestBox(
+                                storeName: '',
+                                address: '',
+                                distance: '',
+                              ),
+                            );
                           },
-                        );
-                      },
-                    ),
+                        )
+                      : ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: controller.store.length > 2
+                              ? controller.store.sublist(0, 3).length
+                              : controller.store.length,
+                          itemBuilder: (context, index) {
+                            final store = controller.store.length > 2
+                                ? controller.store.sublist(0, 3)[index]
+                                : controller.store[index];
+                            final distance = AppHelpers.decimalTwoDigits(
+                                store.distance ?? '0');
+
+                            return NearestBox(
+                              storeName: store.name ?? '',
+                              address: store.address ?? '',
+                              distance: distance,
+                              onTap: () {
+                                controller.currentIndex.value = 2;
+                                controller.boxController.value.closeBox();
+                                controller.selectedLat.value =
+                                    double.parse(store.lat ?? '');
+                                controller.selectedLong.value =
+                                    double.parse(store.long ?? '');
+                                controller.moveStore();
+                              },
+                            );
+                          },
+                        ),
             ),
           ],
         ),
