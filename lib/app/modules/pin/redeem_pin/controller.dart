0ea08177b8890cc43d083f139e6d9e6538a8cd6d
alifzr31 <alifzakya.r@gmail.com>
2024-01-16 15:42:23 +0700

    fixing redeem point

--- lib/app/modules/pin/redeem_pin/controller.dart
+++ lib/app/modules/pin/redeem_pin/controller.dart
@@ -1,8 +1,6 @@
 import 'package:dio/dio.dart' as dio;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:member_rkm/app/core/values/colors.dart';
-import 'package:member_rkm/app/core/values/confirm_dialog.dart';
 import 'package:member_rkm/app/core/values/show_loading.dart';
 import 'package:member_rkm/app/data/providers/pin_provider.dart';
 
@@ -43,49 +41,16 @@ class RedeemPinController extends GetxController {
     final formData = dio.FormData.fromMap({});
 
     print(formData);
-    
+
     showLoading();
 
     await Future.delayed(const Duration(milliseconds: 1500), () {
       Get.back();
-      successDialog(
-        context,
-        title: 'Tukar Poin Berhasil',
-        descChildren: const [
-          TextSpan(
-            text: 'Selamat, anda berhasil menukarkan ',
-          ),
-          TextSpan(
-            text: '100 Poin ',
-            style: TextStyle(
-              color: orangeColor,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          TextSpan(
-            text: 'dengan ',
-          ),
-          TextSpan(
-            text: 'Setrika Cosmos ',
-            style: TextStyle(
-              color: orangeColor,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          TextSpan(
-            text: 'dari toko cabang ',
-          ),
-          TextSpan(
-            text: 'RKM Ahmad Yani (Kota Bandung)',
-            style: TextStyle(
-              color: orangeColor,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ],
-        okPressed: () {
-          Get.back();
-          Get.offAllNamed('/dashboard');
+      Get.offAndToNamed(
+        '/success',
+        arguments: {
+          'title': 'Redeem Poin Berhasil',
+          'subtitle': 'Selamat, anda berhasil menukarkan 100 poin dengan Setrika Cosmos dari toko cabang RKM Ahmad Yani (Kota Bandung)',
         },
       );
     });
